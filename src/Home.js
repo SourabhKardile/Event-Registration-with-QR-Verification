9e6f5fd7b5ccc243d84336efd08e50a6a5575b87
Sourabh Kardile <SOURABHKARDILE47@GMAIL.COM>
2023-09-30 14:31:46 +0530

    QR Pdf Generated

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import "./home.css";
 import { AddDocument, setUpRecaptcha } from "./methods";
 import { useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import QRCode from 'qrcode-generator';
+import jsPDF from 'jspdf';
+
 export default function Home() {
   const navigate = useNavigate();
   const [name, setName] = useState("");
@@ -15,6 +21,8 @@ export default function Home() {
   const [verifyLoader, setVerifyLoader] = useState(false);
   const [phoneVerified, setPhoneVerified] = useState(false);
   const [addDoc, setAddDoc] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [passNo, setPassNo] = useState('');
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -44,8 +52,8 @@ export default function Home() {
         confirm: 0,
       };
       const code = await AddDocument(data);
-      alert(code);
-      navigate(0);
+      setPassNo(code);
+      handleOpen();
     } catch (err) {
       setError(err);
     } finally {
@@ -99,9 +107,51 @@ export default function Home() {
       setVerifyLoader(false);
     }
   };
+  const handleOpen = () => setOpen(true);
+  const handleClose = async() => {await generateQR();setOpen(false); navigate(0)};
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
+  const generateQR = async() => {
+    const qr = QRCode(0, 'H');
+    qr.addData(`${passNo}`);
+    qr.make();
+  
+    const qrCodeUrl = qr.createDataURL(10, 10);
+  
+    const doc = new jsPDF();
+    doc.addImage(qrCodeUrl, 'PNG', 10, 10, 100, 100);
+    doc.save('sample.pdf');
+  }
   return (
     <div className="main">
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 300 }}>
+        <h3 align="center">Your Pass Number is: </h3>
+          <h1 align="center">{passNo}</h1>
+          <p>
+            Name: <b>{name}</b><br />
+            Phone: <b>{phone}</b><br />
+            Address:<b>{address}</b><br />
+
+          </p>
+          <Button onClick={handleClose}>Download QRCode</Button>
+        </Box>
+      </Modal>
       <div className="container">
         <div className="title">Registration</div>
         <div className="content">
